467ed4b8f51c1de5d03a5a23085bccf8d5bc3e72
Roman Denysiuk <r.v.denysiuk@gmail.com>
2026-03-10 15:15:21 +0100

    removed function

--- Program.cs
+++ Program.cs
@@ -5,10 +5,7 @@ Console.BackgroundColor = ConsoleColor.Green;
 Console.ForegroundColor = ConsoleColor.Black;
 Console.WriteLine("Hello, World!");
 
-static double CalculateMin(int[] values)
-{
-    return 0.0;
-}
+
 
 static double CalculateAverage(int[] values)
 {
